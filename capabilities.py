d585f7ca9535218c95b4cac54d0e8bb8a7bd2138
JP-amentum <John.Pollard@amentum.com>
2025-06-11 17:30:50 +0100

    Update capabilities.py

--- capabilities.py
+++ capabilities.py
@@ -155,6 +155,7 @@ elif page == "Home":
     st.title("Welcome to CapabilityConnect!")
     st.text("CapabilityConnect is the primary search and discovery tool for capabilities in Amentum E&E-I")
     st.text("Please select one of the options below")
+    st.radio("Select an option:", ["Home", "Search", "Explorer"])
 
 # --- Search Page ---
 elif page == "Search":
