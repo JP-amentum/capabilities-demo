4d8fa29c69e177ba14ec2add757c229ca58f3ad4
JP-amentum <John.Pollard@amentum.com>
2025-09-10 16:53:20 +0100

    Update capabilities.py

--- capabilities.py
+++ capabilities.py
@@ -128,8 +128,8 @@ st.sidebar.markdown("""
     <hr style="border:none; height: 3px; background-color: #39B54A; margin-top: -10px; margin-bottom: 20px;">
 """, unsafe_allow_html=True)
 
-pages_admin = ["Home", "Admin", "Explorer", "Search", "Tools", "Processes", "Facilities", "US Reachback", "Dashboard", "Feedback"]
-pages_user = ["Home", "Explorer", "Search", "Tools", "Processes", "Facilities", "US Reachback", "Feedback"]
+pages_admin = ["Home", "Admin", "Capability Explorer", "Search", "Tools", "Processes", "Facilities", "US Reachback", "Dashboard", "Feedback"]
+pages_user = ["Home", "Capability Explorer", "Search", "Tools", "Processes", "Facilities", "US Reachback", "Feedback"]
 pages = pages_admin if st.session_state.role == "admin" else pages_user
 
 for p in pages:
@@ -609,6 +609,7 @@ elif st.session_state.page == "Feedback":
     else :
         pass
         
+
 
 
 
